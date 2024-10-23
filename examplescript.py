5789cccc295e54e8c156d036dd1a3f97c8ffdf04
G2L <gregoire.le-lay@u-paris.fr>
2024-10-23 07:40:53 +0200

    better example, more precise todo

--- examplescript.py
+++ examplescript.py
@@ -3,27 +3,29 @@ import pypendentdrop as ppd
 filepath = './src/pypendentdrop/tests/testdata/water_2.tif'
 pxldensity = 57.0
 rhog = 9.812
-roi = [10, 90, 300, 335]
+roi = [10, 90, 300, 335] # use roi=None if you do not care about the ROI
 
-importsuccess, img = ppd.import_image(filepath)
+importsuccess, image = ppd.import_image(filepath)
 
 if not importsuccess:
     raise FileNotFoundError(f'Could not import image at {filepath}')
 
-threshold = ppd.best_threshold(img, roi=roi)
+threshold = ppd.best_threshold(image, roi=roi)
 
-cnt = ppd.find_mainContour(img, threshold, roi=roi)
+contour = ppd.find_mainContour(image, threshold, roi=roi)
 
-estimated_parameters = ppd.estimate_parameters(ppd.image_centre(img), cnt, pxldensity)
+estimated_parameters = ppd.estimate_parameters(ppd.image_centre(image), contour, pxldensity)
 
-estimated_parameters.describe(name='estimated')
+estimated_parameters.set_capillary_length_mm(2.65) # set manually an estimation of the capillary length
 
-opti_success, optimized_parameters = ppd.optimize_profile(cnt, parameters_initialguess=estimated_parameters)
+estimated_parameters.describe(name='estimated')# print the estimated parameters in the console
+
+opti_success, optimized_parameters = ppd.optimize_profile(contour, estimated_parameters)
 
 if not opti_success:
     print('optimization failed :(')
 else:
-    optimized_parameters.describe(name='optimized')
+    optimized_parameters.describe(name='optimized')# print the optimized parameters in the console
 
     print(f'Bond number: {round(optimized_parameters.get_bond(), 3)}')
 
@@ -35,6 +37,6 @@ else:
     from pypendentdrop import plot
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    plot.plot_image_contour(ax1, img, cnt, estimated_parameters, 'estimated', roi=roi)
-    plot.plot_image_contour(ax2, img, cnt, optimized_parameters, 'optimized', roi=roi)
+    plot.plot_image_contour(ax1, image, contour, estimated_parameters, 'estimated', roi=roi)
+    plot.plot_image_contour(ax2, image, contour, optimized_parameters, 'optimized', roi=roi)
     plt.savefig('deleteme_comparison.png', dpi=300)
---
+++ todo.md
@@ -0,0 +1,13 @@
+### TODO
+
+In addition to the todo in the code
+
+### Asap
+* **[LIB]** make `find_mainContour` more pythonic (underscores in name)
+* **[LIB]** make `find_mainContour` accept (image, threshold) signature (in the function, disambiguate between image_centre and image using the shape)
+* **[EX]** comment more the example
+* **[DOC]** give more details as to the pendent drop method and the methods used here
+* **[LIB]** add more user_friendly methods to Parameters (`set_capillary_length`, `set_tip_radius`, etc...)
+
+### Bonus
+* **[GUI]** green isoline for main contour, yellow for other lines
